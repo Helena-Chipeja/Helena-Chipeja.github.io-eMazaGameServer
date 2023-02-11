81f15037f22ebca08ae25a3a0647ed44a751b2c5
Helena-Chipeja <helenaachipeja@gmail.com>
2023-02-11 18:05:33 +0200

    chore: atualização de controllers

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -1,13 +1,31 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Question;
 use Illuminate\Http\Request;
 
 class QuestionController extends Controller
 {
-    public function getData()
+    public function index(Request $request)
     {
-        return response()->json(["Requisição realizada com sucesso..."]);
+        return response()->json(['sucess'=>'Perguntas retornadas com sucesso'], status:200);
+    }
+
+    public function store(Request $request)
+    {
+        return response()->json(['error'=>'Do momento não é possivel adicionar as perguntas'], status:403);
+    }
+
+    public function show(Request $request, Question $question)
+    {
+        return response()->json(['error'=>'Do momento não é possivel mostrar detalhes de uma pergunta'], status:403);
+    }
+    public function update(Request $request, Question $question)
+    {
+        return response()->json(['error'=>'Do momento não é possivel atualizar a pergunta'], status:403);
+    }
+    public function destroy(Request $request, Question $question)
+    {
+        return response()->json(['error'=>'Do momento não é possivel remover a pergunta'], status:403);
     }
 }
--- app/Http/Controllers/SubjectController.php
+++ app/Http/Controllers/SubjectController.php
@@ -2,13 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Subject;
+use App\Traits\HelperTrait;
 use Illuminate\Http\Request;
+
 
 class SubjectController extends Controller
 {
-    public function getData()
+    use HelperTrait;
+    public function index (Request $request)
     {
-        return response()->json(['Requisição realizada com sucesso...']);
+        return $this->processSubjects($request);
+    }
+
+    public function store(Request $request)
+    {
+        return response()->json(['error'=>'Do momento não é possivel adicionar as disciplinas'], status:403);
+    }
+
+    public function show(Request $request, Subject $subject)
+    {
+        return response()->json(['error'=>'Do momento não é possivel mostrar detalhes de uma disciplina'], status:403);
+    }
+    public function update(Request $request, Subject $subject)
+    {
+        return response()->json(['error'=>'Do momento não é possivel atualizar a disciplina'], status:403);
+    }
+    public function destroy(Request $request, Subject $subject)
+    {
+        return response()->json(['error'=>'Do momento não é possivel remover a disciplina'], status:403);
     }
 
 }
